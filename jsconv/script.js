8e730fb4ea7fcba82b91b94e37f0b27cb24dac67
Jaival Patel <pateljaival05@gmail.com>
2022-07-22 19:59:32 -0400

    loadjs

--- jsconv/script.js
+++ jsconv/script.js
@@ -13,9 +13,42 @@ class jsconv {
 
     listjs() {
         var data = fs.readFile(this.filepath, 'utf-8', (err, jsonString) => {
-            console.log(jsonString)       // log data for test purposes
+            if (err) {
+                console.log(err)
+                return false
+            } else {
+                return jsonString
+            }
+                   // log data for test purposes
         })
     }
+
+    jsonReader(filePath=this.filepath, cb) {
+        fs.readFile(filePath, 'utf-8', (err, fileData) => {
+            if (err) {
+                return cb && cb (err)
+            } else {
+                try {
+                    const object = JSON.parse(fileData)
+                    return cb && cb(null, object);
+                } catch (err) {
+                    return cb && cb(err)
+                }
+            }
+        })
+    }
+
+
+    loadjs(key) {
+        this.jsonReader(this.filepath, (err, data) => {
+            if (err) {
+                return err 
+            } else {
+                return data.key
+            }
+        })
+    }
+
 }
 
 
