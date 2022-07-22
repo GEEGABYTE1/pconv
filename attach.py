1fa60d6f84d2f7659b7b2f629c2f9a0f1bb75764
Jaival Patel <pateljaival05@gmail.com>
2022-07-22 19:07:27 -0400

    arch update

--- attach.py
+++ attach.py
@@ -6,11 +6,36 @@ import json
 class Conv:
 
 
-    def attach(self):
-        pass 
+    def __init__(self): 
+        self.command = sys.argv(1)
+
+
+    def save_items(self, filepath, data):
+        with open(filepath, "w") as f:
+            json.dump(data, f)
+
+    def load_items(self, filepath):
+        try:
+            with open(filepath, "r") as f:
+                data = json.load(f)
+                return data
+        except:
+            return "{} ot found".format(filepath)
+
+    def attach(self, file_name, key, data):
+        if type(file_name) != str:
+            return "Invalid Filename"
+        else:
+            file_name += '.json'
+            data[key] = clipboard.paste()
+            self.save_items(file_name, data) # Adding {key: value} data to filepath.json
 
     def spill(self):
         pass 
 
     def load(self):
-        pass 
+        self.load_items()
+
+
+test_conv = Conv()
+test_conv.attach('test')
