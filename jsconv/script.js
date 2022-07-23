2f7ad1c76b40d61e38da52bbf1c4b1aacd71671b
Jaival Patel <pateljaival05@gmail.com>
2022-07-22 21:32:12 -0400

    bug fix of writing to json

--- jsconv/script.js
+++ jsconv/script.js
@@ -14,26 +14,16 @@ class jsconv {
     writejs(key, value) {
         
         const updated_object = this.updateJson(key, value)
-        const object_keys = Object.keys(updated_object)
-        const object_values = Object.values(updated_object)
-        const object_idx = 0 
-        
-        for (object_idx; object_idx <= object_keys.length && object_idx <= object_values.length; object_idx++) {
-            const object_key = object_keys[object_idx]
-            const object_value = object_values[object_idx]
-            const input = {object_key:object_value}
-            let data = JSON.stringify(input)
-            try {
-                fs.writeFileSync(this.filepath, data)
-                console.log('JSON data is saved')
-     
-            } catch (err) {
-                return err
-            }
+        let data = JSON.stringify(updated_object)
+        try {
+            fs.writeFileSync(this.filepath, data)
+            console.log('JSON data is saved')
+    
+        } catch (err) {
+            return err
         }
-
-        
     }
+
 
     readjs() {
         try{
@@ -63,8 +53,10 @@ class jsconv {
 
 
 test = new jsconv('./test.json')
-//test.writejs('key2', 'value2')
+test.writejs('key4', 'works after 3?')
 
+
+/*
 var test_result = test.readjs()
 console.log(test_result) 
-test.updateJson()
+test.updateJson()*/
