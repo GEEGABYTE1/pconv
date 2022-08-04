e59c16d26dc42742d7c0368402ba4ac4ae3dfa3c
Jaival Patel <pateljaival05@gmail.com>
2022-08-04 14:13:21 -0400

    throwing error

--- src/pconv/attach.py
+++ src/pconv/attach.py
@@ -2,51 +2,55 @@ import sys
 import clipboard
 import json 
 
+class NoFilepathFoundError(Exception):
+    pass
 
 class Conv:
 
     def __init__(self, filename):
         self.update(filename)
 
-    def update(self, filename):
-        self.saved_data = filename + '.json'
-        self.save_items(self.saved_data, {'key1':'initialize1'})
+    def update(self, filename):       
+        self.saved_data = filename 
+        self.save_items(self.saved_data, {'key1':'initializer statement from pconv'})
         self.data = self.load_items(self.saved_data)
 
 
 
-    def save_items(self, filepath, data):
+    def save_items(self, filepath, data):   
         with open(filepath, "w") as f:
             json.dump(data, f)
 
-    def load_items(self, filepath):
+    def load_items(self, filepath):           
         try:
             with open(filepath, "r") as f:
                 data = json.load(f)
                 return data
         except:
-            return "{} not found".format(filepath)
+            raise NoFilepathFoundError("{} not found".format(filepath))  # Throw new error
 
-    def attach(self, file_name, key, value):
+    def attach(self, file_name, key, value): # user func
         if type(file_name) != str:
-            return "Invalid Filename"
+            raise NoFilepathFoundError("{} not found".format(file_name)) # Throw new error
         else:
             self.data[key] = value
-            self.save_items(self.saved_data, self.data) # Adding {key: value} data to filepath.json
+            self.save_items(self.saved_data, self.data) 
 
-    def spill(self):
+    def spill(self): #user func
         try:
             return self.data
         except:
-            return 'Data cannot be loaded'
+            return None
 
-    def load(self, key):
+    def load(self, key):    # user func
         if key in self.data:
             return self.data[key]
         else:
-            return 'Key not found'
+            return None
 
 
-#pconv = Conv('test')
-#pconv.attach('test', 'myface', 'value2')
+pconv = Conv('test.json')
+pconv.attach('test', 'myface', 'value2')
+results = pconv.spill()
+print(results)
 
